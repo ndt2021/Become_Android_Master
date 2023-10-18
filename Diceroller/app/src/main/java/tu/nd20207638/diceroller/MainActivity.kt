16d1d2e22cbc903162899003020e689c32607ee7
tú <nguyenductu2002tu@gmail.com>
2023-10-18 22:35:47 +0700

    commit Dice Roll
    commit Dice Roll Android app lần 1
    10:35 PM  18/10/2023

---
+++ Diceroller/app/src/main/java/tu/nd20207638/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package tu.nd20207638.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button2)
+        rollButton.setOnClickListener {
+            rollDice()
+
+/*            val toast = Toast.makeText(this,"Dice Roll!", Toast.LENGTH_LONG)
+           toast.show()
+*/
+/*            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+ */
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+
+    class Dice(private val numSides: Int){
+        fun roll(): Int{
+            return (1..numSides).random()
+        }
+    }
+}
