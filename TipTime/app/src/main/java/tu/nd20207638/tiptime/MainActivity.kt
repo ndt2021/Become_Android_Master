fa55e13db20774065da81bbe3b9122d70401a3cb
tú <nguyenductu2002tu@gmail.com>
2023-10-24 23:51:12 +0700

    Tip app lan 1
    11:50PM 24/10/2023

---
+++ TipTime/app/src/main/java/tu/nd20207638/tiptime/MainActivity.kt
@@ -0,0 +1,11 @@
+package tu.nd20207638.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
